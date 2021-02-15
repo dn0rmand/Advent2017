30dedd83a71c0753991f0f5d1812fc58d6f456aa
Dominique NORMAND <dominique@normand.us>
2021-02-15 10:13:37 -0500

    day 2 done + added timing
    UI very much in needs of help :)

--- Advent2017/Days/Day.swift
+++ Advent2017/Days/Day.swift
@@ -33,9 +33,32 @@ class Day {
 		output.Print(message: "--- Day \(d) ---")
 		output.Print(message: "")
 		
+		let startTime = CACurrentMediaTime()
 		let p1 = self.part1();
 		output.Print(message:"Answer to part1 is \(p1)")
 		let p2 = self.part2();
 		output.Print(message:"Answer to part1 is \(p2)")
+		var timeElapsed = Int((CACurrentMediaTime() - startTime) * 1000000) // in μs
+		
+		let μs      = timeElapsed % 1000
+		timeElapsed = (timeElapsed - μs) / 1000
+		let ms 		= timeElapsed % 1000
+		timeElapsed = (timeElapsed - ms) / 1000
+		let seconds = timeElapsed % 60
+		timeElapsed = (timeElapsed - seconds) / 60
+		let minutes = timeElapsed % 60
+		timeElapsed = (timeElapsed - minutes) / 60
+		let hours 	= timeElapsed
+
+		var format = ""
+		if hours > 0 { format = "\(hours) hours" }
+		if minutes > 0 { format = "\(format) \(minutes) minutes" }
+		if seconds > 0 { format = "\(format) \(seconds) seconds" }
+		if ms > 0 { format = "\(format) \(ms) ms" }
+		if μs > 0 { format = "\(format) \(μs) μs" }
+		
+		if format.count == 0 { format = "so very fast!" }
+		
+		output.Print(message: "Executed in \(format)")
 	}
 }
--- Advent2017/Days/day2.swift
+++ Advent2017/Days/day2.swift
@@ -10,13 +10,65 @@ import Foundation
 class Day2: Day {
 	override func getDay() -> Int { return 2 }
 	
+	private var _values : [[Int]]? = nil
+	
+	var values : [[Int]] {
+		get {
+			if (_values == nil) {
+				var vals = [[Int]]()
+				
+				for line in input {
+					var row = [Int]()
+					
+					for num in line.split(separator: "\t") {
+						row.append(Int(num)!)
+					}
+					
+					row.sort()
+					vals.append(row)
+				}
+				
+				_values = vals
+			}
+			return _values!
+		}
+	}
+	
 	override func part1() -> String
 	{
-		return "0"
+		var total = 0
+		
+		for row in values {
+			let min = row[row.startIndex]
+			let max = row[row.index(before: row.endIndex)]
+			total += max-min
+		}
+		
+		return "\(total)"
 	}
 	
 	override func part2() -> String
 	{
-		return "0"
+		var total = 0
+		
+		for row in values {
+			var found = false
+			for i in 1...row.count {
+				let max = row[row.index(row.startIndex, offsetBy: row.count-i)]
+				for min in row {
+					if min >= max {
+						break
+					}
+					if max % min == 0 {
+						total += max/min
+						found = true
+						break
+					}
+				}
+				if found { break }
+			}
+		}
+		
+		return "\(total)"
 	}
 }
